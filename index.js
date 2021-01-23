905b669e62f1f176d1637517f137d8738c4ed4ac
Kevin Ho <traderkev54@yahoo.com>
2021-01-23 21:35:58 +0800

    change of api

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-$("#form").submit(function (e) {
-    e.preventDefault()
+$(document).ready(function () {
 
-    var query = $("#search").val()
+    var API_KEY = 'AIzaSyAeEkr2Kd5WLw1E3t6uaMgmcjyqJBBfTIU'
 
-    var API_KEY = '91e67499c338b646b3590f73a5696583'
+    var video = ''
 
-    var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&query=${query}'
-    console.log(url)
+    var videos = $("#videos")
+
+    $("#form").submit(function (event) {
+        event.preventDefault()
+        alert("Please wait. Form submitted")
+
+        var search = $("#search").val()
+
+        videoSearch(API_KEY, search, 21)
+    })
+
+    function videoSearch(key, search, maxResults) {
+
+        $("#videos").empty()
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + API_KEY + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+            console.log(url)
+
+            data.items.forEach(item => {
+                video = `
+            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.if.videoId" frameborder="0" allowfullscreen></iframe>`
 
 
-    $.get(url, function (data) {
+            })
 
-        $("#result").html('')
 
-        console.log(data)
+                })
 
-        data.organic_results.forEach(res => {
-            result = `
-      
-      <h1>${res.title}</h1><br><a target="_blank href="${res.url}">${res.url}</a>
-      <p>${res.snippet}</p>`
+            });
 
-            $("#result").append(result)
         })
-
-    });
-
-})
 
 // $("#form").submit(function(){
 
