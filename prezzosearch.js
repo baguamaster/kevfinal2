847a17f5f93d5c62c521c19884a859f86144301b
Kevin Ho <traderkev54@yahoo.com>
2021-01-24 21:04:16 +0800

    added radio buttons to augment search terms

--- prezzosearch.js
+++ prezzosearch.js
@@ -2,7 +2,7 @@ $("#form").submit(function (e) {
     e.preventDefault()
 
     var query = $("#search").val()
-   
+    
     var API_KEY = '91e67499c338b646b3590f73a5696583'
 
     var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Appt`
