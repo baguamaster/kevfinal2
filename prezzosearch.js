872dbedf3dd78e2f13d178483b1a5570cbc01cb8
Kevin Ho <traderkev54@yahoo.com>
2021-01-24 18:18:34 +0800

    streamline webpages

---
+++ prezzosearch.js
@@ -0,0 +1,32 @@
+$("#form").submit(function (e) {
+    e.preventDefault()
+
+    var query = $("#search").val()
+   
+    var API_KEY = '91e67499c338b646b3590f73a5696583'
+
+    var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Apdf`
+
+
+    console.log(url)
+
+
+    $.get(url, function (data) {
+
+        $("#result").html('')
+
+        console.log(data)
+
+        data.organic_results.forEach(res => {
+            result = `
+      
+      <h1>${res.title}</h1><br><a target="_blank" href="${res.url}">${res.url}</a>
+      <p>${res.snippet}</p>`
+
+            $("#result").append(result)
+        })
+
+    });
+
+})
+
