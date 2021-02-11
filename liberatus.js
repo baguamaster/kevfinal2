c1afb13d2fd71a20adebdf569d3728b1e678be18
baguamaster <traderkev54@yahoo.com>
2021-02-11 16:42:39 +0800

    JS cleanup thru Jlinter

--- liberatus.js
+++ liberatus.js
@@ -1,9 +1,9 @@
 $("#form").submit(function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    var query = $("#search").val()
+    var query = $("#search").val();
 
-    var API_KEY = '91e67499c338b646b3590f73a5696583'
+    var API_KEY = '91e67499c338b646b3590f73a5696583';
 
 
     //var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Apdf`
@@ -13,75 +13,45 @@ $("#form").submit(function (e) {
 
     //RIGHT NOW, IT IS MORE MANUAL IN THE SENSE THAT 'URL' IS REPEATED, BUT THE FUNCTIONALITY IS NOT AFFECTED. 
     //NEXT STEP IS TO MAKE IT LOOK NICER BY PUTTING STUFF INTO FUNCTIONS, SO THAT CAN JUST CALL SAID FUNCTIONS EASILY.
-    var rd1 = document.getElementById("rd1")
-    var rd2 = document.getElementById("rd2")
-    var rd3 = document.getElementById("rd3")
-    var rd4 = document.getElementById("rd4")
+    var rd1 = document.getElementById("rd1");
+    var rd2 = document.getElementById("rd2");
+    var rd3 = document.getElementById("rd3");
+    var rd4 = document.getElementById("rd4");
 
     //APP CHECKS FOR WHAT IS SELECTED. SO IF RD1 WAS SELECTED, FUNCTION EXECUTES ETC.
     if (rd1.checked == true)
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Apdf`
+        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Apdf`;
 
     else if (rd2.checked == true)
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Appt`
+        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Appt`;
 
     else if (rd3.checked == true)
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Axls`
+        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Axls`;
 
     else if (rd4.checked == true)
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Adoc`
+        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Adoc`;
 
-    console.log(url)
+    console.log(url);
 
 
-    //var filetype = $("filetype").val();
 
-    /*
-    var filetype = document.getElementById('filetype').value; {
 
-        if (document.getElementById('pdf').checked) {
-            filetype_value = document.getElementById('pdf').value;
-        }
-
-        if (document.getElementById('ppt').checked) {
-            filetype_value = document.getElementById('ppt').value;
-        }
-
-        if (document.getElementById('xls').checked) {
-            filetype_value = document.getElementById('xls').value;
-        }
-    };
-    
-    if (filetype_value = pdf) {
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Apdf`
-    }
-
-    else if (filetype_value = ppt) {
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Appt`
-    }
-
-    else if (filetype_value = xls) {
-        var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + `&query=${query}` + `+filetype%3Axls`
-    }
-    */
-
-    
 
     $.get(url, function (data) {
 
-        $("#result").html('')
-            console.log(data)
+        $("#result").html('');
+        console.log(data);
 
-    data.organic_results.forEach(res => {
-        result = `
+        data.organic_results.forEach(res => {
+            result = `
       
       <h1>${res.title}</h1><br><a target="_blank" href="${res.url}">${res.url}</a>
-      <p>${res.snippet}</p>`
+      <p>${res.snippet}</p>`;
 
-            $("#result").append(result)
-        })
+            $("#result").append(result);
+        });
 
     });
 
-})
+});
 
