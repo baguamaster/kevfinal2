9ce6357a381291daf0247bf80bd453b3cd460e86
baguamaster <traderkev54@yahoo.com>
2021-06-17 08:00:58 +0000

    new book search  function

---
+++ book.js
@@ -0,0 +1,20 @@
+google.books.load();
+
+function previewBook() {
+    var isbn = $("#isbn").val();
+    console.log(isbn)
+    //sessionStorage.setItem("isbn",isbn); //session storage was used as workaround but not good to use
+    return(isbn);
+}
+
+ function initialize() {
+    previewBook();
+    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+    var isbn = previewBook()
+    //var isbnNum = sessionStorage.getItem("isbn");
+    console.log("ISBN:"+ isbn)
+    viewer.load("ISBN:"+ isbn);
+    
+}
+
+
