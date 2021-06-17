3a64ba2473e4f3e417a360a099967bcdf8056611
baguamaster <traderkev54@yahoo.com>
2021-06-17 08:51:55 +0000

    global format document

--- book.js
+++ book.js
@@ -4,17 +4,17 @@ function previewBook() {
     var isbn = $("#isbn").val();
     console.log(isbn)
     //sessionStorage.setItem("isbn",isbn); //session storage was used as workaround but not good to use
-    return(isbn);
+    return (isbn);
 }
 
- function initialize() {
+function initialize() {
     previewBook();
     var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
     var isbn = previewBook()
     //var isbnNum = sessionStorage.getItem("isbn");
-    console.log("ISBN:"+ isbn)
-    viewer.load("ISBN:"+ isbn);
-    
+    console.log("ISBN:" + isbn)
+    viewer.load("ISBN:" + isbn);
+
 }
 
 
